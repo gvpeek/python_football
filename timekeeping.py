faa8fd3e7fe7bcf73a4ef546f4eecf2868c93028
gvpeek <gvpeek@gmail.com>
2012-02-21 20:03:36 +0000

    initial commit

---
+++ timekeeping.py
@@ -0,0 +1,44 @@
+'''
+Created on Feb 21, 2012
+
+@author: peekgv
+'''
+
+#===============================================================================
+# >>> number_of_quarters = 4
+# >>> from collections import deque
+# >>> d = deque()
+# >>> for i in range(number_of_quarters):
+#    d.append(Clock())
+# 
+# >>> ct = None    
+# >>> if not ct:
+#    ct = d.popleft()
+# >>> while ct.minutes > 0 or ct.seconds > 0:
+#    ct.run_clock()
+#===============================================================================
+
+
+from copy import deepcopy
+
+class Clock(object):
+    "Basic Clock"
+    def __init__(self, quarter_length=15):
+        self.minutes = deepcopy(quarter_length)
+        self.seconds = 00
+        self.clicks = 0
+
+    def run_clock(self):
+
+        if self.minutes == 0 and self.seconds == 0:
+            return False
+          
+        self.clicks += 1
+    
+        if (self.clicks % 2) > 0:
+            self.minutes -= 1
+            self.seconds = 30
+        else:
+            self.seconds = 0
+
+        return self.minutes, self.seconds
