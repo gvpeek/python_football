c7a1220f4457426d9c33b03df7ca219f33232622
gmoneyp-projectlocker@yahoo.com <gmoneyp-projectlocker@yahoo.com@597de6de-3200-433e-baf0-c9780b96bf38>
2012-02-22 04:59:31 +0000

    refactored to use datetime object

--- timekeeping.py
+++ timekeeping.py
@@ -11,36 +11,22 @@ Created on Feb 21, 2012
 # >>> for i in range(number_of_quarters):
 #    d.append(Clock())
 # 
-# >>> ct = None    
-# >>> if not ct:
-#    ct = d.popleft()
-# >>> while ct.minutes > 0 or ct.seconds > 0:
-#    ct.run_clock()
+# Example:
+# >>> c = Clock()
+# >>> while c.time_remaining:
+#     t = c.run_clock()
+#     str(t)[2:7]
 #===============================================================================
 
 
-from copy import deepcopy
+from datetime import timedelta
 
 class Clock(object):
     "Basic Clock"
     def __init__(self, quarter_length=15):
-        self.minutes = deepcopy(quarter_length)
-        self.seconds = 00
-        self.clicks = 0
-        self.active = True
+        self.time_remaining = timedelta(seconds=(quarter_length*60))
 
     def run_clock(self):
-
-        if self.active:
-            self.clicks += 1
+        self.time_remaining -= timedelta(seconds=30)
     
-            if (self.clicks % 2) > 0:
-                self.minutes -= 1
-                self.seconds = 30
-            else:
-                self.seconds = 0
-        
-        if self.minutes == 0 and self.seconds == 0:
-            self.active = False
-
-        return self.minutes, self.seconds, self.active
+        return self.time_remaining
