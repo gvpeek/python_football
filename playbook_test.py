a25b82ef8a9b8ef04320d38a7e378fba2ddc9a4a
gvpeek <gvpeek@gmail.com>
2013-03-30 16:34:04 +0000

    completed refactoring plays and moving to playbook.py. Changed field goal algorithm, as well as return algorithms.

--- playbook_test.py
+++ playbook_test.py
@@ -5,8 +5,7 @@ Created on Mar 25, 2013
 '''
 
 from play import Team
-from playbook import Playbook
-from game import Field
+
 
 t1=Team('Chicago','Bears')
 t2=Team('Arizona','Cardinals')
@@ -21,10 +20,10 @@ turnover_ctr=0.0
 for i in range(1,20):
     for play in t1.playbook:
         count+=1
-        off_yd, turnover, ret_yd = play.run(t1.skills,t2.skills,3)
+        off_yd, turnover, ret_yd = play.run(t1.skills,t2.skills,0)
         if turnover:
             turnover_ctr +=1
-        print play.id, off_yd, turnover, ret_yd
+        print play.id, play.name, '--', off_yd, turnover, ret_yd
 print count, turnover_ctr/count
         
 print
@@ -35,8 +34,8 @@ turnover_ctr=0.0
 for i in range(1,20):
     for play in t2.playbook:
         count+=1
-        off_yd, turnover, ret_yd = play.run(t2.skills,t1.skills,0)
+        off_yd, turnover, ret_yd = play.run(t2.skills,t1.skills,t1.home_field_advantage)
         if turnover:
             turnover_ctr +=1
-        print play.id, off_yd, turnover, ret_yd
+        print play.id, play.name, '--',  off_yd, turnover, ret_yd
 print count, turnover_ctr/count
