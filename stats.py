134723c8b307a275d91873396a7f80456b43431e
gmoneyp-projectlocker@yahoo.com <gmoneyp-projectlocker@yahoo.com@597de6de-3200-433e-baf0-c9780b96bf38>
2013-04-17 23:25:21 +0000

    adding stats and team files to repo

---
+++ stats.py
@@ -0,0 +1,31 @@
+'''
+Created on Apr 14, 2013
+
+@author: George
+'''
+
+
+class StatBook():
+    def __init__(self):
+        self.offense_stats = {'score' : 0,
+                              'total_yards' : 0,
+                              'pass_att' : 0,
+                              'pass_comp' : 0,
+                              'completion_pct' : 0.0,
+                              'pass_yards' : 0,
+                              'pass_td' : 0,
+                              'intercepted' : 0,
+                              'sacked' : 0,
+                              'rush_att' : 0,
+                              'rush_yards' : 0,
+                              'rush_td' : 0,
+                              'fumbles' : 0,
+                              'fg_att' : 0,
+                              'fg' : 0,
+                              'xp_att' : 0,
+                              'xp' : 0,
+                              'conv_att' : 0,
+                              'conv' : 0,
+                              'punts' : 0,
+                              'punt_yards' : 0
+                              }
---
+++ team.py
@@ -0,0 +1,32 @@
+'''
+Created on Mar 30, 2013
+
+@author: George
+'''
+from random import randint
+
+from playbook import Playbook
+from stats import StatBook
+
+class Team():
+    "Team"
+    def __init__(self, city, nickname):
+#        self.id = get_next_team_id()
+        self.city = city
+        self.nickname = nickname
+        self.skills = {'qb': float(randint(60,90)),
+                       'rb': float(randint(60,90)),
+                       'wr': float(randint(60,90)),
+                       'ol': float(randint(60,90)),
+                       'dl': float(randint(60,90)),
+                       'lb': float(randint(60,90)),
+                       'cb': float(randint(60,90)),
+                       's': float(randint(60,90)),
+                       'sp': float(randint(60,90))}
+        
+        self.home_field_advantage = float(randint(1,3))
+        self.playbook = Playbook()
+        self.stats = StatBook()
+        
+        self.primary_color = (randint(0,255),randint(0,255),randint(0,255))
+        self.secondary_color = (randint(0,255),randint(0,255),randint(0,255))
