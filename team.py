c62bca40db6f924a753e818a30c5feee9b542a7b
gmoneyp-projectlocker@yahoo.com <gmoneyp-projectlocker@yahoo.com@597de6de-3200-433e-baf0-c9780b96bf38>
2013-04-30 12:00:19 +0000

    began coding league module, added league stats to team

--- team.py
+++ team.py
@@ -4,15 +4,22 @@ Created on Mar 30, 2013
 @author: George
 '''
 from random import randint
+from collections import OrderedDict
 
 from playbook import Playbook
 from stats import StatBook
 from coach import Coach
 
+team_id = 0
+global team_id
+
 class Team():
     "Team"
-    def __init__(self, city, nickname,human=False):
-#        self.id = get_next_team_id()
+    def __init__(self, city, nickname,human=False,id=None):
+        if id:
+            self.id = id
+        else:
+            self.id = self.get_next_team_id()
         self.city = city
         self.nickname = nickname
         self.human_control = human
@@ -34,3 +41,18 @@ class Team():
         
         self.primary_color = (randint(0,255),randint(0,255),randint(0,255))
         self.secondary_color = (randint(0,255),randint(0,255),randint(0,255))
+        
+        self.league_stats = OrderedDict([('wins', 0),
+                            ('losses' , 0),
+                            ('ties' , 0),
+                            ('pct' , 0.0),
+                            ('points' , 0),
+                            ('opp' , 0),
+                            ('win_opp' , []),
+                            ('loss_opp' , [])
+                            ])
+        
+    def get_next_team_id(self):
+        global team_id
+        team_id += 1
+        return team_id 
