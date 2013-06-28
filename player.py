9ceeb7b408464cade0cacf47f684cea96ae442e3
gmoneyp-projectlocker@yahoo.com <gmoneyp-projectlocker@yahoo.com@597de6de-3200-433e-baf0-c9780b96bf38>
2013-06-28 23:42:24 +0000

    added sqlalchemy to store players in sqllite db

--- player.py
+++ player.py
@@ -4,12 +4,58 @@ Created on May 27, 2013
 @author: George
 '''
 from math import pow, floor
-
 from random import randint, choice
+
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 import names
 
-position_types = ['QB']*3+['RB']*5+['WR']*5+['TE']*3+['OG']*4+['OT']*4+['C']*3+['DT']*4+['DE']*4+['LB']*6+['S']*4+['CB']*6+['K']+['P']
+Base = declarative_base()
+
+class DBPLayer(Base):
+    __tablename__ = 'players'
+
+    id = Column(Integer, primary_key=True)
+    first_name  = Column(String)
+    last_name = Column(String)
+    age = Column(Integer)
+    position = Column(String)
+    constitution = Column(Integer)
+    retired = Column(Boolean)
+    apex_age = Column(Integer)
+    growth_rate = Column(Integer)
+    declination_rate = Column(Integer)
+    ratings = Column(Integer) # need to convert to dict
+
+    def __init__(self, player):
+        self.id = player.id              
+        self.first_name = player.first_name      
+        self.last_name = player.last_name       
+        self.age = player.age             
+        self.position = player.position        
+        self.constitution = player.constitution    
+        self.retired = player.retired         
+        self.apex_age = player.apex_age        
+        self.growth_rate = player.growth_rate     
+        self.declination_rate = player.declination_rate
+        self.ratings = player.ratings['rating']         
+
+    def __repr__(self):
+        return "<Player('%s','%s','%s','%s', '%s')>" % (self.first_name, self.last_name, self.age, self.position, self.ratings)
+
+class PlayerDatabase():
+    def __init__(self,base):
+        self.engine = create_engine('sqlite:///:memory:', echo=True)
+        self.Session = sessionmaker(bind=self.engine)
+        self.record = DBPLayer
+        
+        base.metadata.create_all(self.engine)
+
+
+
+#position_types = ['QB']*3+['RB']*5+['WR']*5+['TE']*3+['OG']*4+['OT']*4+['C']*3+['DT']*4+['DE']*4+['LB']*6+['S']*4+['CB']*6+['K']+['P']
 position_types = ['QB']+['RB']+['WR']+['OL']+['DL']+['LB']+['S']+['CB']+['K']+['P']+['SP']
 
 player_id = 0
@@ -42,16 +88,23 @@ class Player():
     
 class PlayerManagement():
     def __init__(self,new_annual_players=33):
+        self.db = PlayerDatabase(Base)
         self.new_annual_players = new_annual_players
         self.min_max_ratings = [(14,(20,50)), # (age, (min,max))
                                 (18,(30,60)),
                                 (22,(45,75)),
                                 (99,(60,90))] 
         self.players = self._create_players(self.new_annual_players)
-        
 
     def _create_players(self,number):
-        return [Player() for x in xrange(number)]
+        new_players = [Player() for x in xrange(number)]
+        
+        db_players =[self.db.record(player) for player in new_players]
+        session = self.db.Session()
+        session.add_all(db_players)
+        session.commit()
+        
+        return new_players
     
     def _age_players(self,years=1):
         for y in xrange(years):
@@ -79,7 +132,13 @@ class PlayerManagement():
     def advance_year(self):
         self._age_players()
         self.players.extend(self._create_players(self.new_annual_players))
+
+
+
+        
+        
     
+
 ##### testing
 
 pm=PlayerManagement(new_annual_players=75)
@@ -89,14 +148,16 @@ pm=PlayerManagement(new_annual_players=75)
 #          player.age, \
 #          player.position, \
 #          player.ratings['rating']
-for zz in xrange(30):
-    pm.advance_year()
-#    print "\n", "New Year..."
-pm.players.sort(reverse=True, key=lambda t: t.ratings['rating'])
-for player in pm.players:
-    if not player.retired:
-        print player.first_name, \
-              player.last_name, \
-              player.age, \
-              player.position, \
-              player.ratings['rating']
+#for zz in xrange(30):
+#    pm.advance_year()
+#    list=0
+#    print
+#    pm.players.sort(reverse=True, key=lambda t: t.ratings['rating'])
+#    for player in pm.players:
+#        if not player.retired and list < 10: # would be more efficient as a generator
+#            list += 1
+#            print player.first_name, \
+#                  player.last_name, \
+#                  player.age, \
+#                  player.position, \
+#                  player.ratings['rating']
