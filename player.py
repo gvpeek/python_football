7c4a8e6dd7168200e15136505f41c36f022445b2
gmoneyp-projectlocker@yahoo.com <gmoneyp-projectlocker@yahoo.com@597de6de-3200-433e-baf0-c9780b96bf38>
2013-06-29 16:13:57 +0000

    modified age_player method to use database. removed player array variable. removed id reference in Player class so it is now set by database.

--- player.py
+++ player.py
@@ -30,7 +30,7 @@ class DBPLayer(Base):
     ratings = Column(Integer) # need to convert to dict
 
     def __init__(self, player):
-        self.id = player.id              
+#        self.id = player.id              
         self.first_name = player.first_name      
         self.last_name = player.last_name       
         self.age = player.age             
@@ -43,37 +43,32 @@ class DBPLayer(Base):
         self.ratings = player.ratings['rating']         
 
     def __repr__(self):
-        return "<Player('%s','%s','%s','%s', '%s')>" % (self.first_name, self.last_name, self.age, self.position, self.ratings)
+        return "<Player('%s','%s','%s','%s','%s','%s','%s')>" % (self.id, 
+                                                                 self.first_name, 
+                                                                 self.last_name, 
+                                                                 self.age, 
+                                                                 self.position, 
+                                                                 self.ratings, 
+                                                                 self.retired)
 
 class PlayerDatabase():
     def __init__(self,base):
-        self.engine = create_engine('sqlite:///:memory:', echo=True)
+        self.engine = create_engine('sqlite:///:memory:')
         self.Session = sessionmaker(bind=self.engine)
-        self.record = DBPLayer
+        self.record_class = DBPLayer
         
         base.metadata.create_all(self.engine)
 
 
 
-#position_types = ['QB']*3+['RB']*5+['WR']*5+['TE']*3+['OG']*4+['OT']*4+['C']*3+['DT']*4+['DE']*4+['LB']*6+['S']*4+['CB']*6+['K']+['P']
-position_types = ['QB']+['RB']+['WR']+['OL']+['DL']+['LB']+['S']+['CB']+['K']+['P']+['SP']
-
-player_id = 0
-global player_id
-
 class Player():
-    def __init__(self,id=None):
-        if id:
-            self.id = id
-        else:
-            self.id = self.get_next_player_id() 
+    def __init__(self,position_types):
         self.first_name = names.first_name()
         self.last_name = names.last_name()
         self.age = 11
         self.position = choice(position_types)
         
         self.constitution = randint(20,45)
-#        self.retirement_age = (self.constitution - self.age) + 10
         self.retired = False
         self.apex_age = (floor(((32 * 100) * pow(randint(5,100),-.5)) / 100) + 18)
         self.growth_rate = randint(1,4)
@@ -81,57 +76,56 @@ class Player():
         
         self.ratings ={ 'rating': randint(20,45) }
         
-    def get_next_player_id(self):
-        global player_id
-        player_id += 1
-        return player_id 
-    
 class PlayerManagement():
     def __init__(self,new_annual_players=33):
         self.db = PlayerDatabase(Base)
+        self.position_types = ['QB']+['RB']+['WR']+['OL']+['DL']+['LB']+['S']+['CB']+['K']+['P']+['SP']
         self.new_annual_players = new_annual_players
         self.min_max_ratings = [(14,(20,50)), # (age, (min,max))
                                 (18,(30,60)),
                                 (22,(45,75)),
                                 (99,(60,90))] 
-        self.players = self._create_players(self.new_annual_players)
+        
+        self._create_players(self.new_annual_players)
 
     def _create_players(self,number):
-        new_players = [Player() for x in xrange(number)]
+        new_players = [Player(self.position_types) for x in xrange(number)]
         
-        db_players =[self.db.record(player) for player in new_players]
+        db_players =[self.db.record_class(player) for player in new_players]
         session = self.db.Session()
         session.add_all(db_players)
         session.commit()
+        session.close()
         
         return new_players
     
     def _age_players(self,years=1):
+        session = self.db.Session()
+        
         for y in xrange(years):
-            for player in self.players:
-#                if not player.retired and player.age >= player.retirement_age:
-#                    player.retired = True
-                if not player.retired:
-                    player.age += 1
-                    if player.age <= player.apex_age:
-                        player.ratings['rating'] += randint(1,player.growth_rate)
-                    else:
-                        player.ratings['rating'] -= randint(3,player.declination_rate)
-                    for age,ratings in self.min_max_ratings:
-                        if player.age <= age:
-                            self._check_rating_range(player, ratings)
-                            break
+            for player in session.query(self.db.record_class).filter_by(retired=False):
+                player.age += 1
+                if player.age <= player.apex_age:
+                    player.ratings += randint(1,player.growth_rate)
+                else:
+                    player.ratings -= randint(3,player.declination_rate)
+                for age,ratings in self.min_max_ratings:
+                    if player.age <= age:
+                        self._check_rating_range(player, ratings)
+                        break
+                session.commit()
+        session.close()
                             
     def _check_rating_range(self,player,range):
-        if player.ratings['rating'] < min(range):
+        if player.ratings < min(range):
             player.retired = True
-        elif player.ratings['rating'] > max(range):
-            player.ratings['rating'] = max(range)
+        elif player.ratings > max(range):
+            player.ratings = max(range)
         
     
     def advance_year(self):
         self._age_players()
-        self.players.extend(self._create_players(self.new_annual_players))
+        self._create_players(self.new_annual_players)
 
 
 
@@ -142,22 +136,10 @@ class PlayerManagement():
 ##### testing
 
 pm=PlayerManagement(new_annual_players=75)
-#for player in pm.players:
-#    print player.first_name, \
-#          player.last_name, \
-#          player.age, \
-#          player.position, \
-#          player.ratings['rating']
-#for zz in xrange(30):
-#    pm.advance_year()
-#    list=0
-#    print
-#    pm.players.sort(reverse=True, key=lambda t: t.ratings['rating'])
-#    for player in pm.players:
-#        if not player.retired and list < 10: # would be more efficient as a generator
-#            list += 1
-#            print player.first_name, \
-#                  player.last_name, \
-#                  player.age, \
-#                  player.position, \
-#                  player.ratings['rating']
+for zz in xrange(10):
+    print 'year',(zz+1)
+    pm.advance_year()
+
+session=pm.db.Session()
+for player in session.query(pm.db.record_class).filter_by(retired=False).order_by(pm.db.record_class.ratings):
+    print player
