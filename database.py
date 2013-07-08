024b35e14f5c0c0948f2f699436ff71ff9758d84
gvpeek <gvpeek@gmail.com>
2013-07-08 11:30:04 +0000

    began work on adding coach/team relationship, added statbook class (not yet fuctional)

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ Created on Jun 29, 2013
 @author: George Peek
 '''
 
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, PickleType, Float
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, PickleType, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -70,8 +70,9 @@ class DBTeam(Base):
     skills = Column(PickleType)
     home_field_advantage = Column(Float)
 #    playbook = Column(Boolean)
-#    apex_age = Column(Integer)
-#    growth_rate = Column(Integer)
+#    stats = Column(Integer)
+    coach_id = Column(Integer, ForeignKey('coaches.id'))
+#@TODO    coach = relationship("Coach", uselist=False, backref="parent")
 #    declination_rate = Column(Integer)
     
     def __init__(self, team):
@@ -82,8 +83,7 @@ class DBTeam(Base):
         self.home_field_advantage = team.home_field_advantage
 #        self.playbook = Playbook()
 #        self.stats = StatBook()
-#        self.coach = Coach()
-#        self.coach.practice_plays(self.playbook,self.skills)
+        self.coach = team.coach
 #        
 #        self.primary_color = (randint(0,255),randint(0,255),randint(0,255))
 #        self.secondary_color = (randint(0,255),randint(0,255),randint(0,255))
@@ -95,9 +95,75 @@ class DBCoach():
     skill = Column(Integer)
     play_probabilities = Column(PickleType)
     fg_dist_probabilities= Column(PickleType)
+    team = Column(Integer, ForeignKey('teams.id'))
     
     def __init__(self, coach):
         self.skill = coach.skill
         self.play_probabilities = coach.play_probabilities 
         self.fg_dist_probabilities = coach.fg_dist_probabilities
-                   
+        
+class DBStatBook():
+    __tablename__ = 'stats'
+    
+    id = Column(Integer, primary_key=True)
+    total_yards = Column(Float)
+    pass_att = Column(Float)
+    pass_comp = Column(Float)
+    completion_pct = Column(Float)
+    pass_yards = Column(Float)
+    pass_td = Column(Float)
+    intercepted = Column(Float)
+    sacked = Column(Float)
+    rush_att = Column(Float)
+    rush_yards = Column(Float)
+    rush_td = Column(Float)
+    fumbles = Column(Float)
+    fg_att = Column(Float)
+    fg = Column(Float)
+    xp_att = Column(Float)
+    xp = Column(Float)
+    conv_att = Column(Float)
+    conv = Column(Float)
+    punts = Column(Float)
+    punt_yards = Column(Float)
+    punt_returns = Column(Float)
+    punt_return_yards = Column(Float)
+    kickoffs = Column(Float)
+    kickoff_yards = Column(Float)
+    kickoff_touchbacks = Column(Float)
+    kick_returns = Column(Float)
+    kick_return_yards = Column(Float)
+    safeties = Column(Float)
+    
+    def __init__(self, stats):
+        pass
+#        self.stats = {'score
+#                      'total_yards
+#                      'pass_att
+#                      'pass_comp
+#                      'completion_pct
+#                      'pass_yards
+#                      'pass_td
+#                      'intercepted
+#                      'sacked
+#                      'rush_att
+#                      'rush_yards
+#                      'rush_td
+#                      'fumbles
+#                      'fg_att
+#                      'fg
+#                      'xp_att
+#                      'xp
+#                      'conv_att
+#                      'conv
+#                      'punts
+#                      'punt_yards
+#                      'punt_returns
+#                      'punt_return_yards': 0.0,
+#                      'kickoffs
+#                      'kickoff_yards
+#                      'kickoff_touchbacks
+#                      'kick_returns
+#                      'kick_return_yards': 0.0,
+#                      'safeties': 0.0
+#                   
